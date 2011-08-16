b46c67aae76eed2da500d0045356283fb9a55cbb
Dmitry Kurinskiy <name.alari@gmail.com>
2011-08-16 14:17:01 +0400

    first commit

---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,34 @@
+dataSource {
+    pooled = true
+/*    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""*/
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+          dbCreate = "update"
+          url = 'jdbc:mysql://localhost/litclub'
+            driverClassName = 'com.mysql.jdbc.Driver'
+            //dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb"
+        }
+    }
+}
