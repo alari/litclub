888ceb429bfb442ad0e3f050af1fdd93bb2fc055
Dmitry Kurinskiy <name.alari@gmail.com>
2011-09-05 19:09:49 +0400

    ??? cleaning

---
+++ grails-app/taglib/litclub/TalkTagLib.groovy
@@ -0,0 +1,20 @@
+package litclub
+
+class TalkTagLib {
+  static namespace = "talk"
+
+  def talkService
+  def springSecurityService
+
+  def newCount = {attrs->
+    if(!springSecurityService.isLoggedIn()) return;
+
+    long personId = (long)springSecurityService.getCurrentUser().id
+
+    if(attrs.talkId) {
+      out << talkService.getTalkNewCount(personId, attrs.talkId as long)
+    } else {
+      out << talkService.getNewCount(personId)
+    }
+  }
+}
---
+++ test/unit/litclub/TalkTagLibSpec.groovy
@@ -0,0 +1,11 @@
+package litclub
+
+import spock.lang.*
+import grails.plugin.spock.*
+
+class TalkTagLibSpec extends TagLibSpec {
+
+  def "feature method"() {
+
+  }
+}
